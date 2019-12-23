0e8fa996d806e720257431b3bf55adc511df9c98
jelleas <jelle.van.assema@gmail.com>
2019-12-23 21:06:59 +0100

    solution in comments

--- tests/unit_test.py
+++ tests/unit_test.py
@@ -2,23 +2,19 @@ from basis import interpret
 
 import pytest
 import os
+import re
 
 def create(tests):
-    def test_func(test_file, solution_file):
-        with open(solution_file) as f:
-            solution = [line.strip() for line in f]
+    def test_func(test_file):
+        with open(test_file) as f:
+            solution = re.findall(r"//(.*)", f.read())
         assert interpret(test_file) == solution
 
-    return pytest.mark.parametrize("test_file,solution_file", tests)(test_func)
+    return pytest.mark.parametrize("test_file", tests)(test_func)
 
 
 for root, dirs, files in os.walk("tests/files"):
-    tests = []
-    for f in files:
-        if f.endswith(".code"):
-            test_file = os.path.join(root, f)
-            solution_file = test_file.replace(".code", ".sol")
-            tests.append((test_file, solution_file))
+    tests = [os.path.join(root, f) for f in files if f.endswith(".code")]
 
     if tests:
         test_name = "test_" + os.path.basename(root)
